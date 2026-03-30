dcddc058e508d33ca0b139fe6ee56fcd4102c31e
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-03-30 14:37:26 -0300

    naooo

--- layout/src/app/app.routes.ts
+++ layout/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Farmador } from './pages/farmador/farmador';
+import { Mogador } from './pages/mogador/mogador';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: 'farmador', component: Farmador },
+    { path: 'mogador', component: Mogador },
+];
--- layout/src/app/pages/home/home.ts
+++ layout/src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -10,15 +12,15 @@ export class Home {
   requisicao=[
     {
       tecnologia:'HTML',
-      descricao:'Estrutura da página'
+      descricao:'Se a internet fosse um corpo, o HTML seriam os ossos. É através dele que damos significado e hierarquia à informação, transformando linhas de código na base de tudo o que você vê e interage online.'
     },
     {
       tecnologia:'CSS',
-      descricao:'Estilização da página'
+      descricao:'Se o HTML é a fundação, o CSS é o design de interiores. É a ferramenta que nos permite controlar cada pixel na tela, trazendo personalidade, harmonia visual e usabilidade para o mundo digital.'
     },
     {
       tecnologia:'JavaScript',
-      descricao:'Lógica da página'
+      descricao:'Enquanto o HTML estrutura e o CSS embeleza, o JavaScript executa. É a linguagem que possibilita a interatividade, o processamento de dados e o comportamento inteligente que define a web moderna.'
     }
     
   ];
