138e2d6d3bc00feedb8dd0ced410bf282f099070
Luiza Swarowsky <luiza_swarowsky@estudante.sesisenai.org.br>
2026-03-30 14:46:01 -0300

    s

--- layout/src/app/app.routes.ts
+++ layout/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Farmador } from './pages/farmador/farmador';
 import { Mogador } from './pages/mogador/mogador';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-     { path: 'farmador', component: Farmador },
+    { path: 'farmador', component: Farmador },
     { path: 'mogador', component: Mogador },
+    { path: '', component: Home },
 ];
